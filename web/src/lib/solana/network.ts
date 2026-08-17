11417bcde639aabdcc02b72179ee3980c6447332
Treekapat420 <pcoltsfan23@aol.com>
2026-08-17 19:26:41 -0400

    Update network.ts

--- web/src/lib/solana/network.ts
+++ web/src/lib/solana/network.ts
@@ -3,7 +3,7 @@ import { clusterApiUrl } from "@solana/web3.js";
 export type KodiakNetwork = "devnet" | "mainnet";
 
 export const KODIAK_MAINNET_CPMM_CONFIG_ID =
-  "D4FPEruKEHrG5TenZ2mpDGEfu1iUvTiqBxvpU8HLBvC2";
+  "EUZHCdd8H7nueb2wLpxUqyuemdbe8TUxfCWcxVfARvgw";
 
 function requestedNetwork(): KodiakNetwork {
   const value =

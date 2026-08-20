139310e2715ea770f59b20a93dbf6beeadb2769f
Treekapat420 <pcoltsfan23@aol.com>
2026-08-19 21:52:35 -0400

    Update admin.ts

--- web/src/lib/admin.ts
+++ web/src/lib/admin.ts
@@ -1,5 +1,5 @@
 export const KODIAK_ADMIN_WALLETS = new Set([
-  "HFo47xm7JmnyseZM6sa9YBuLE2V7VqSRtfsxrBNj91NX",
+  "Af1JaQiQjpkmcpexDXQXH81XN97GHYhnJ3nnAf9R8iU9",
 ]);
 
 export function isKodiakAdminWallet(
